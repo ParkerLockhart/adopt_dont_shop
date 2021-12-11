a6ed1e7f01472b270923ae32f4195ce5fe5c4b21
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-10 20:54:31 -0600

    Feat:application

---
+++ spec/models/application_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe Application, type: :model do
+  describe 'relationships' do
+    it { should have_many(:application_pets) }
+    it { should have_many(:pets).through(:application_pets)}
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:status) }
+  end
+
+  describe 'class methods' do
+  end
+
+  describe 'instance methods' do
+  end
+end
