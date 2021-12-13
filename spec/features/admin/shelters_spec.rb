d83e305c1d422c0d0acb7258e5349a26bee10425
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-13 11:52:58 -0600

    Fix: db seed issue

--- spec/features/admin/shelters_spec.rb
+++ spec/features/admin/shelters_spec.rb
@@ -6,7 +6,18 @@ RSpec.describe 'admin shelter index page' do
     @shelter_2 = Shelter.create(name: 'RGV animal shelter', city: 'Harlingen, TX', foster_program: false, rank: 5)
     @shelter_3 = Shelter.create(name: 'Fancy pets of Colorado', city: 'Denver, CO', foster_program: true, rank: 10)
 
-    visit '/admin'
+    @pet_1 = @shelter_1.pets.create(name: 'Mr. Pirate', breed: 'tuxedo shorthair', age: 5, adoptable: false)
+    @pet_2 = @shelter_1.pets.create(name: 'Clawdia', breed: 'shorthair', age: 3, adoptable: true)
+    @pet_3 = @shelter_3.pets.create(name: 'Lucille Bald', breed: 'sphynx', age: 8, adoptable: true)
+    @pet_4 = @shelter_1.pets.create(name: 'Ann', breed: 'ragdoll', age: 5, adoptable: true)
+
+    @app_1 = Application.create!(name: "Amy", street_address: "5223 Lovely Ln.", city: "Starfield", state: "TX", zip_code: "78230", description: "I want to take care of a pet who needs help.", status: "Pending")
+    @app_2 = Application.create!(name: "Jason", street_address: "7229 Warp Dr.", city: "Orion", state: "CO", zip_code: "80918", description: "description", status: "In Progress")
+
+    @app_1.pets << @pet_2
+    @app_2.pets << @pet_3
+
+    visit '/admin/shelters'
   end
 
   it 'shows all shelters in reverse alphabetical order' do
@@ -20,10 +31,12 @@ RSpec.describe 'admin shelter index page' do
   end
 
   it 'shows all shelters with pending applications' do
-    #look at each shelter
-    #look at each of the pets
-    #look at each pet's applications to see if status == pending
-    #Shelter.where(pets.where(Application.pending))
-
+    # look at each shelter
+    # look at each of the pets
+    # look at each pet's applications to see if status == pending
+    # Shelter.where(pets.where(Application.pending))
+    within("#admin-shelters-pending") do
+      expect(page).to have_content(@shelter_1.name)
+    end
   end
 end
