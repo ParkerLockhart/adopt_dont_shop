455537cd02c3cc3a6cca4268b4cd329a6b497401
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-13 11:58:32 -0600

    Fix: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-ApplicationPets.destroy_all
+ApplicationPet.destroy_all
 Pet.destroy_all
 Shelter.destroy_all
 Application.destoy_all
